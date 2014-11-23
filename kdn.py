6c6c486cc5101273cf380e2ca2a28223f5454b13
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-11-23 01:09:34 +0100

    improved more, now it looks quite fine :)

--- kdn.py
+++ kdn.py
@@ -3,44 +3,76 @@ from click import echo
 from click import style
 
 
+class Config(object):
+
+    def __init__(self):
+        self.verbose = False
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
 @click.group()
 @click.option('--rss/--no-rss', default=False)
-def kdncli():
+@click.option('--verbose', is_flag=True)
+@click.option('--save-directory', type=click.Path())
+@pass_config
+def cli(config, verbose, rss, save_directory):
     """ What's new on KDNuggets """
-    pass
+    """ Simple program that reads stories from KDNuggets in a command line """
+    if verbose:
+        echo(" Verbose mode enabled")
+    if not rss:
+        echo(" Parsing pure HTML for content")
+    if save_directory is None:
+        save_directory = '.'
+    config.verbose = verbose
+    config.save_directory = save_directory
 
 
-@kdncli.command(name="podcasts")
+@cli.command(name="podcasts")
 def podcasts():
     """ List podcasts """
-    echo(" List podcasts ")
+    echo(" List podcasts")
     pass
 
 
-@kdncli.command(name="view")
+@cli.command(name="view")
 def go():
-    """ Go to the story on KDNuggets """
-    echo(" Story: ")
+    """ Go to the particular story on KDNuggets """
+    echo(" Story:")
     pass
 
 
-@kdncli.command(name="stories")
+@cli.command(name="stories")
 @click.option('--sort_by', '-s', default='newest',
-              type=click.Choice(['newest', 'best']), help='sort type')
+              type=click.Choice(['newest', 'best']),
+              help='How news should be sorted')
 @click.option('--limit', '-l', default=10, type=click.INT,
               help='number of top stories to show')
-def stories(sort_by, limit):
+@pass_config
+def stories(config, sort_by, limit):
     """ List stories """
-    echo(" List stories ")
+    echo(" Listing KDN stories...")
+    if (config.verbose):
+        echo("Running stories(sort_by=" + str(sort_by) +
+             ", limit=" + str(limit) + ")")
+        echo("Save directory is %s" % config.save_directory)
     pass
 
 
-@click.command()
-def usage():
-    """ Simple program that reads stories from KDNuggets in a command line """
-    echo(style(' KDNuggets reader v. 0.1 ', fg='yellow'))
+@cli.command(name="jobs")
+def jobs():
+    """ List jobs """
+    echo(style(' Listing news on jobs as seen on KDNuggets...', fg='yellow'))
+
+
+@cli.command(name="academic")
+def academic():
+    """ List academic topics """
+    echo(style(' KDNuggets academic', fg='yellow'))
 
 
 if __name__ == '__main__':
-    echo(" KDNuggets - for data hackers ")
-    usage()
+    echo(" KDNuggets - command line tool for data hackers")
+    cli()
